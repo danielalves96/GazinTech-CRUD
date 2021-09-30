8ce3f362303b83df744f2af78d4637a58e1a2e47
Daniel Alves <daniel.alves@madeiramadeira.com.br>
2021-09-29 22:40:13 -0300

    Reset api

--- backend/app/Exceptions/Handler.ts
+++
@@ -1,8 +0,0 @@
-import Logger from '@ioc:Adonis/Core/Logger'
-import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
-
-export default class ExceptionHandler extends HttpExceptionHandler {
-  constructor() {
-    super(Logger)
-  }
-}
--- backend/database_scripts/create_database.sql
+++
@@ -1,3 +0,0 @@
-create database crud_gazin_database;
-
-\c crud_gazin_database
